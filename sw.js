ce2a43b43214ac797d5d9bea590be845c5a6275f
Schnark <listenleser@gmail.com>
2019-02-01 10:04:53 +0100

    Frauentag Berlin

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 (function (worker) {
 "use strict";
 
-var VERSION = 'v2.10',
+var PREFIX = 'calender',
+	VERSION = '2.11',
 	FILES = [
 		'index.html',
 		'style.css',
@@ -50,7 +51,7 @@ var VERSION = 'v2.10',
 
 worker.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open(VERSION).then(function (cache) {
+		caches.open(PREFIX + ':' + VERSION).then(function (cache) {
 			return cache.addAll(FILES);
 		})
 	);
@@ -60,7 +61,7 @@ worker.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keys) {
 			return Promise.all(keys.map(function (key) {
-				if (key !== VERSION) {
+				if (key.indexOf(PREFIX + ':') === 0 && key !== PREFIX + ':' + VERSION) {
 					return caches.delete(key);
 				}
 			}));
